1b56c2178b2d14ea202568bfcee20e199ad08e52
L200183043 <45007772+L200183043@users.noreply.github.com>
2018-12-30 19:46:59 +0700

    Modul Praktikum 12
    Modul Praktikum 12

---
+++ 12_keg2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+print "<!DOCTYPE html>"
+print
+print """<html>
+<head><title>Selamat Datang</title></head>
+<body>
+<table>
+	<tr>
+		<td rowspan='6'>FOTO</td>
+		<td><b><font size='5'>Data diri</font><b></td>
+	</tr>
+	<tr>
+		<td>Nama </td>
+		<td>:</td>
+		<td>Aria widiyo Noviyanto</td>
+	</tr>
+	<tr>
+		<td>Alamat tinggal </td>
+		<td>:</td>
+		<td>Pontianak</td>
+	</tr>
+	<tr>
+		<td>Tempat, tanggal lahir </td>
+		<td>:</td>
+		<td>Pontianak, 20 November 1999</td>
+	</tr>
+	<tr>
+		<td>Tempat wisata favorit </td>
+		<td>:</td>
+		<td>Gunung</td>
+	</tr>
+	<tr>
+		<td>Motto </td>
+		<td>:</td>
+		<td>Try to Believe in Something that Can Not be Seen with Your Eyes</td>
+	</tr>
+</table>
+</body>
+</html>"""
